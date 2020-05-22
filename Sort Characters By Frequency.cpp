494ad15d5370338daa0238de4d5c79cf2553987b
Jaskaran Batra <jskrnbtr@gmail.com>
2020-05-22 14:44:27 +0530

    Update Sort Characters By Frequency.cpp
    uisng c++

--- Sort Characters By Frequency.cpp
+++ Sort Characters By Frequency.cpp
@@ -9,3 +9,28 @@ class Solution:
         for k,v in arr:
             ans=ans+(v*k)
         return ans
+
+//Alternative for cpp using pq
+
+class Solution {
+public:
+    string frequencySort(string s) 
+    {
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.size();i++)
+        mp[s[i]]++;
+        priority_queue<pair<int,char>>pq;
+        for(auto x:mp)
+        pq.push({x.second,x.first});
+        string st="";
+        while(!pq.empty())
+        {
+            pair<int,char>x=pq.top();
+            int temp=x.first;
+            while(temp--)
+            st+=x.second;
+            pq.pop();
+        }
+        return st;
+    }
+};
